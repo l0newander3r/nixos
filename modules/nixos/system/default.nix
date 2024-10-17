fd58c17ee00342bc16319d9c6af494669f47ebcd
Lone Wanderer <iam@lonewanderer.org>
2024-10-17 20:03:44 +0000

    fix: added devenv caching

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -60,10 +60,12 @@ in {
       use-xdg-base-directories = true;
       substituters = [
         "https://cache.nixos.org?priority=10"
+        "https://devenv.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
